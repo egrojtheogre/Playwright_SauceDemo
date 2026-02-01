da4d6e93e21ca31b0d2f57a4c490d01e5275b27f
egrojtheogre <egrojtheogre@gmail.com>
2026-02-01 15:51:11 -0600

    adding some purchase flows to test_purchase_flow file

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,12 +6,27 @@ class InventoryPage:
         self.burger_menu_button = page.locator("#react-burger-menu-btn")
         self.logout_link = page.locator("#logout_sidebar_link")
         self.backpack_add_button = page.locator("[data-test='add-to-cart-sauce-labs-backpack']")
+        self.onesie_add_button = page.locator("[data-test='add-to-cart-sauce-labs-onesie']")
+        self.bike_light_add_button = page.locator("[data-test='add-to-cart-sauce-labs-bike-light']")
+        self.fleece_jacket_add_button = page.locator("[data-test='add-to-cart-sauce-labs-fleece-jacket']")
         self.cart_badge = page.locator(".shopping_cart_badge")
         self.cart_link = page.locator(".shopping_cart_link")
         self.checkout_button = page.locator("[data-test='checkout']")
+        self.product_sort = page.locator("[data-test='product-sort-container']")
+
+
 
     def add_backpack_to_cart(self):
         self.backpack_add_button.click()
+
+    def add_onesie_to_cart(self):
+        self.onesie_add_button.click()
+
+    def add_bike_light_to_cart(self):
+        self.bike_light_add_button.click()
+
+    def add_fleece_jacket_to_cart(self):
+        self.fleece_jacket_add_button.click()
 
     def go_to_cart(self):
         self.cart_link.click()
@@ -22,3 +37,10 @@ class InventoryPage:
         # 2. Click the logout link (Playwright waits for it to be visible!)
         self.logout_link.click()
 
+    def sort_by_price_low_to_high(self):
+        # Instead of clicking twice, we tell Playwright to select by the 'value'
+        # The values in SauceDemo are: 'az', 'za', 'lohi', 'hilo'
+        self.product_sort.select_option("lohi")
+
+    def sort_by_price_high_to_low(self):
+        self.product_sort.select_option("hilo")
--- tests/test_purchase_flow.py
+++ tests/test_purchase_flow.py
@@ -30,3 +30,62 @@ def test_full_purchase_flow(page):
     expect(checkout.complete_header).to_have_text("Thank you for your order!")
     # expect(checkout.complete_header).to_have_text("WRONG HEADER TEXT")
     pass
+
+
+def test_full_purchase_cheap(page):
+    # Initialize all pages
+    login = LoginPage(page)
+    inventory = InventoryPage(page)
+    checkout = CheckoutPage(page)
+
+    # 1. Login
+    login.navigate()
+    login.login("standard_user", "secret_sauce")
+
+    # 2. Sort price low to high
+    inventory.sort_by_price_low_to_high()
+
+    # 3. Add to cart
+    inventory.add_onesie_to_cart()
+    inventory.add_bike_light_to_cart()
+
+    # 4. go to cart
+    inventory.go_to_cart()
+    page.locator("#checkout").click()  # Quick click on checkout button
+
+    # 5. Complete Information
+    checkout.fill_checkout_info("John", "Connor", "10121")
+
+    # 6. Final Step & Verification
+    checkout.finish_checkout()
+    expect(checkout.complete_header).to_have_text("Thank you for your order!")
+    pass
+
+def test_full_purchase_expensive(page):
+    # Initialize all pages
+    login = LoginPage(page)
+    inventory = InventoryPage(page)
+    checkout = CheckoutPage(page)
+
+    # 1. Login
+    login.navigate()
+    login.login("standard_user", "secret_sauce")
+
+    # 2. Sort price low to high
+    inventory.sort_by_price_high_to_low()
+
+    # 3. Add to cart
+    inventory.add_fleece_jacket_to_cart()
+    inventory.add_backpack_to_cart()
+
+    # 4. go to cart
+    inventory.go_to_cart()
+    page.locator("#checkout").click()  # Quick click on checkout button
+
+    # 5. Complete Information
+    checkout.fill_checkout_info("John", "Connor", "10121")
+
+    # 6. Final Step & Verification
+    checkout.finish_checkout()
+    expect(checkout.complete_header).to_have_text("Thank you for your order!")
+    pass
